cb60c5dccdfb64e61b9c0bc0af02335aa6eb54a9
Avrgael250 <maskallergy@gmail.com>
2025-09-18 20:18:41 -0600

    Corrección
    Me equivoque, y no leí la indicación, ahora si, la hice bien

--- Practica1/Ejercicio/Ejercicio1/Ejercicio2.js
+++ Practica1/Ejercicio/Ejercicio1/Ejercicio2.js
@@ -1,5 +1,9 @@
-const cuadrado = (numero) => numero * numero;
+const cuadrado = (numero) => 5 * 5;
+const cuadrado2 = (numero) => 8 * 8;
+const cuadrado3 = (numero) => 12 * 12;
 
-document.write(`El cuadrado es ${cuadrado(5)}`)
+document.write(`El cuadrado es ${cuadrado(5)}` + "<br>");
+document.write(`El cuadrado es ${cuadrado2(8)}` + "<br>");
+document.write(`El cuadrado es ${cuadrado3(12)}` + "<br>");
 //console.log();
 //muestra en consola en probando con 3 numeros diferentes

68b2663914dd9544b2a928843f05f5ea1c21b097
Avrgael250 <maskallergy@gmail.com>
2025-09-25 23:04:02 -0600

    Update Ejercicio 2
    Lo que me imprimia, no era la indicacion del profe, asi que tuve que corregirlo, y hacer mi mayor esfuerzo.
    Falle, pero aprendí.

--- Practica3/Ejercicio2.js
+++ Practica3/Ejercicio2.js
@@ -1,28 +1,22 @@
-// Si el nombre de usuario es "admin", la promesa se resuelve con "Acceso concedido", si no,
-// se rechaza con "Acceso denegado".
-const promesa = new Promise((resolve, reject) => {
-    verificarUsuario("admin")
-        .then(res => resolve(res))
-        .catch(err => reject(err));
-        
-});
-
-
-// Usa .then() y .catch() para manejar el resultado
-verificarUsuario = (username) =>{
-    return new Promise((resolve, reject) =>{
-        if(username === "admin"){
-            resolve("Acceso concedido");
+function verificarUsuario(username) {
+    return new Promise((resolve, reject) => {
+        if(username === "admin") {
+            resolve({ acceso: true, mensaje: "Acceso concedido"});
         } else {
-            reject("Acceso denegado");
-        }
-    })
+            reject({ acceso: false, mensaje: "Acceso denegado"});
+        }     
+    });
 }
 
-verificarUsuario("admin")
-.then(res => document.write(res))
-.catch(err => document.write(err)); // Acceso concedido
 
-verificarUsuario("Willyrex")
-.then(res => document.write(res))
-.catch(err => document.write(err)); // Acceso denegado
+verificarUsuario("admin")
+    .then(res => {
+        if(res.acceso) {
+            console.log(res.mensaje);
+    }
+})
+    .catch(err => {
+        if(!err.acceso) {
+            console.log(err.mesaje);
+    }
+});

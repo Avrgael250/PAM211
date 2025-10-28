3354cc080ced5b48d1ff408dda8c8258c1b6ae6b
Avrgael250 <maskallergy@gmail.com>
2025-10-27 18:51:36 -0600

    ScrollView : Completada
    Creación del ScrollView y de sus interacciones al scrollear

--- INTRO/screens/ScrollViewScreen.js
+++ INTRO/screens/ScrollViewScreen.js
@@ -1,10 +1,283 @@
 import { Text, StyleSheet, View, ScrollView } from 'react-native'
-import React from 'react'
+import Reac, { Component } from 'react'
 
 export default function ScrollViewScreen() {
+
     return (
-        <View>
-            <Text>Proximamente...</Text>
+        <View style = {styles.container}>
+            <ScrollView style = {styles.scrollArea} contentContainerStyle = {styles.scrollContent} showsHorizontalScrollIndicator = {true}>
+
+                <Text style={styles.title}> ScrollView </Text>
+
+            <Text style={styles.textSong}> Blue </Text>
+            <Text style={styles.text}> 
+Mm, mm
+Mm
+I try to live in black and white, but I'm so blue
+I'd like to mean it when I say I'm over you
+But that's still not true (blue)
+And I'm still so blue, oh
+I thought we were the same (we were the same)
+Birds of a feather, now I'm ashamed
+I told you a lie, désolé, mon amour
+I'm trying my best, don't know what's in store
+Open up the door (blue)
+In the back of my mind, I'm still overseas
+A bird in a cage, thought you were made for me
+I try (I'm not what) to live in black and white
+But I'm so blue (but I'm not what you need)
+I'd like (not what you need) to mean it when I say I'm over you
+But that's still not true, true
+And I'm still so blue (and it's not true)
+I'm true blue, true blue
+I'm true blue
+(Blue) mm, mm
+Mm
+Ah, ah
+Ah
+You were born bluer than a butterfly
+Beautiful and so deprived of oxygen
+Colder than your father's eyes
+He never learned to sympathize with anyone
+I don't blame you
+But I can't change you
+Don't hate you (don't hate you)
+But we can't save you (but we can't save you)
+You were born reaching for your mother's hands
+Victim of your father's plans to rule the world
+Too afraid to step outside
+Paranoid and petrified of what you've heard
+But they could say the same 'bout me
+I sleep 'bout three hours each night
+Means only 21 a week now
+Now
+And I could say the same 'bout you
+Born blameless, grew up famous too
+Just a baby born blue now
+Now
+I don't blame you
+But I can't change you
+Don't hate you
+But we can't save you
+Ooh
+It's over now
+It's over now
+It's over now
+Ah-ah
+Ah
+But when can I hear the next one?            </Text>
+
+            <Text style={styles.textSong}> Do I Wanna Know? </Text>
+            <Text style={styles.text}>
+
+Have you got colour in your cheeks?
+Do you ever get that fear that you can't shift the type
+That sticks around like summat in your teeth?
+Are there some aces up your sleeve?
+Have you no idea that you're in deep?
+I've dreamt about you nearly every night this week
+How many secrets can you keep?
+'Cause there's this tune I found that makes me think of you somehow
+And I play it on repeat until I fall asleep
+Spillin' drinks on my settee
+(Do I wanna know) if this feelin' flows both ways?
+(Sad to see you go) was sorta hopin' that you'd stay
+(Baby, we both know) that the nights were mainly made
+For sayin' things that you can't say tomorrow day
+Crawlin' back to you
+Ever thought of callin' when you've had a few?
+'Cause I always do
+Maybe I'm too
+Busy being yours to fall for somebody new
+Now I've thought it through
+Crawlin' back to you
+So have you got the guts?
+Been wonderin' if your heart's still open
+And if so, I wanna know what time it shuts
+Simmer down and pucker up
+I'm sorry to interrupt, it's just I'm constantly on the cusp
+Of tryin' to kiss you
+I don't know if you feel the same as I do
+But we could be together
+If you wanted to
+(Do I wanna know) if this feelin' flows both ways?
+(Sad to see you go) was sorta hopin' that you'd stay
+(Baby, we both know) that the nights were mainly made
+For sayin' things that you can't say tomorrow day
+Crawlin' back to you (crawlin' back to you)
+Ever thought of callin' when you've had a few? (You've had a few)
+'Cause I always do ('cause I always do)
+Maybe I'm too (maybe I'm too busy)
+Busy being yours to fall for somebody new (being yours)
+Now I've thought it through
+Crawlin' back to you
+(Do I wanna know) if this feelin' flows both ways?
+(Sad to see you go) was sorta hopin' that you'd stay
+(Baby, we both know) that the nights were mainly made
+For sayin' things that you can't say tomorrow day
+(Do I wanna know?) Too busy bein' yours to fall
+(Sad to see you go) ever thought of callin', darlin'?
+(Do I wanna know?) Do you want me crawlin' back to you? </Text>
+
+            <Text style={styles.textSong}> Like a Stone </Text>
+            <Text style={styles.text}>On a cold, wet afternoon
+In a room full of emptiness
+By a freeway I confess
+I was lost in the pages
+Of a book full of death
+Reading how we'll die alone
+And if we're good we'll lay to rest
+Anywhere we want to go
+In your house
+I long to be
+Room by room
+Patiently
+I'll wait for you there
+Like a stone
+I'll wait for you there
+Alone
+And on my death bed I will pray
+To the gods and the angels
+Like a pagan to anyone
+Who will take me to heaven
+To a place I recall
+I was there so long ago
+The sky was bruised, the wine was bled
+And there you led me on
+In your house
+I long to be
+Room by room
+Patiently
+I'll wait for you there
+Like a stone
+I'll wait for you there
+Alone
+Alone
+And on I read
+Until the day was gone
+And I sat in regret
+Of all the things I've done
+For all that I've blessed
+And all that I've wronged
+In dreams until my death
+I will wander on
+In your house
+I long to be
+Room by room
+Patiently
+I'll wait for you there
+Like a stone
+I'll wait for you there
+Alone
+Alone</Text>
+
+            <Text style={styles.textSong}> Instant Crush </Text>
+            <Text style={styles.text}>I didn't want to be the one to forget
+I thought of everything I'd never regret
+A little time with you is all that I get
+That's all we need because it's all we can take
+One thing I never see the same when your 'round
+I don't believe in him, his lips on the ground
+I wanna take you to that place in the "Roche"
+But no one gives us any time anymore
+He asked me once if I'd look in on his dog
+You made an offer for it, then you ran off
+I got this picture of us, gets in my head
+And all I hear is the last thing that you said
+"I listened to your problems
+Now listen to mine"
+I didn't want to anymore, oh oh oh
+And we will never be alone again
+'Cause it doesn't happen every day
+Kinda counted on you being a friend
+Can I give it up or give it away?
+Now I thought about what I wanna say
+But I never really know where to go
+So I chained myself to a friend
+'Cause I know it unlocks like a door
+And we will never be alone again
+'Cause it doesn't happen every day
+Kinda counted on you being a friend
+Can I give it up or give it away?
+Now I thought about what I wanna say
+But I never really know where to go
+So I chained myself to a friend
+Some more again
+It didn't matter what they wanted to see
+He thought he saw someone that looked just like me
+That summer memory that just never dies
+We worked too long and hard to give it no time
+He sees right through me, it's so easy with lies
+Cracks in the road that I would try and disguise
+He runs his scissors at the seem in the wall
+He cannot break it down or else he would fall
+One thousand lonely stars hiding in the cold
+Take it, I don't wanna sing anymore
+"I listened to your problems
+Now listen to mine"
+I didn't want to anymore, oh oh oh
+And we will never be alone again
+'Cause it doesn't happen every day
+Kinda counted on you being a friend
+Can I give it up or give it away?
+Now I thought about what I wanna say
+But I never really know where to go
+So I chained myself to a friend
+'Cause I know it unlocks like a door
+And we will never be alone again
+'Cause it doesn't happen every day
+Kinda counted on you being a friend
+Can I give it up or give it away?
+Now I thought about what I wanna say
+But I never really know where to go
+So I chained myself to a friend
+'Cause I know it unlocks like a door
+I don't understand, don't get upset
+I'm not with you
+We're swimming around
+It's all I do, when I'm with you
+And we will never be alone again
+'Cause it doesn't happen every day
+Kinda counted on you being a friend
+Can I give it up or give it away?
+Now I thought about what I wanna say
+But I never really know where to go
+So I chained myself to a friend
+'Cause I know it unlocks like a door
+And we will never be alone again
+'Cause it doesn't happen every day
+Kinda counted on you being a friend
+Can I give it up or give it away?
+Now I thought about what I wanna say
+But I never really know where to go
+So I chained myself to a friend
+'Cause I know it unlocks like a door</Text>
+
+            <Text style={styles.textSong}>  </Text>
+            <Text style={styles.text}>ScrollViewScreen</Text>
+
+            <Text style={styles.textSong}>  </Text> 
+            <Text style={styles.text}>ScrollViewScreen</Text>
+
+            <Text style={styles.textSong}>  </Text>
+            <Text style={styles.text}>ScrollViewScreen</Text>
+
+            <Text style={styles.textSong}>  </Text>
+            <Text style={styles.text}>ScrollViewScreen</Text>
+
+            <Text style={styles.textSong}>  </Text>
+            <Text style={styles.text}>ScrollViewScreen</Text>
+
+            <Text style={styles.textSong}>  </Text>
+            <Text style={styles.text}>ScrollViewScreen</Text>
+
+            <Text style={styles.textSong}>  </Text>
+            <Text style={styles.text}>ScrollViewScreen</Text>
+
+            <Text style={styles.textSong}>  </Text>
+            <Text style={styles.text}>ScrollViewScreen</Text>
+
+            </ScrollView>
         </View>
     )
 }
@@ -12,22 +285,67 @@ export default function ScrollViewScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: '#4b6448ff',
     },
-    content: {
-        padding: 20,
+
+    scrollArea: {
+        flex: 1,
+
     },
-    title: {
-        fontSize: 24,
-        marginBottom: 20,
+    
+    scrollContent: {
+        alignItems: 'center',
+        paddingVertical: 20,
+    },
+
+    item: {
+        backgroundColor:'#a5ce5dff',
+        color: '#6257a2ff',
+        padding: 12,
+        marginVertical: 8,
+        borderRadius: 10,
         textAlign: 'center',
     },
-    box: {
-        backgroundColor: '#e0e0e0',
-        padding: 20,
-        marginBottom: 10,
-        borderRadius: 5,
+
+    title: {
+        fontsize: 30,
+        fontWeight: 'bold',
+        color: '#C9C9C9',
+        textAlign: 'center',
+        marginBottom: 20,
     },
-    boxText: {
+
+    subtitle: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        color: '#C9C9C9',
+        textAlign: 'center',
+        backgroundColor: '#C9C9C9',
+        borderRadius: 10,
+        marginVertical: 12,
+        paddingVertical: 12,
+        paddingHorizontal: 10,
+        width: '95%',
+        alignSelf: 'center',
+    },
+
+    text: {
         fontSize: 16,
+        color: '#C9C9C9',
+        lineHeight: 24,
+        textAlign: 'justify',
+        marginBottom: 15,
+        padding: 10,
+    },
+
+    textSong: {
+        fontSize: 16,
+        color: '#d7ffcfff',
+        lineHeight: 24,
+        textAlign: 'justify',
+        marginBottom: 15,
+        padding: 10,
+        backgroundColor: '#094b14ff',
+        borderRadius: 10,
     }
 })
